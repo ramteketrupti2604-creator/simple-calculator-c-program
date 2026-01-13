bcd4a51fcd260416987e5c95092213f1e4e8c494
ramteketrupti2604-creator <ramteketrupti2604@gmail.com>
2026-01-13 11:12:55 +0530

    Add files via upload

---
+++ simplecalculator.c
@@ -0,0 +1,9 @@
+//write a program of simple calculator
+#include <stdio.h>
+int main() {
+    int a=5,b=3;
+    char op='+';
+    if(op=='+') printf("%d",a+b);
+    else if(op=='-') printf("%d",a-b);
+    return 0;
+}
